0dd2239ed8696063a6bc2b97719e91c93d62a258
mitchellbarclay <37090779+mitchellbarclay@users.noreply.github.com>
2023-08-03 15:26:05 +1200

    update

--- m/w/char/editor.js
+++ m/w/char/editor.js
@@ -2,17 +2,62 @@ let canvas;
 let canvasContainer;
 let sliders = [];
 let targetValues = [];
-let toggleButton; // This will replace slider16
+let toggleButton;
 let bg_color = [0];
 let shirtR, shirtG, shirtB;
 
+const startSyllables = [
+'Be', 'Ne', 'Ab', 
+'Del', 'Sh', 'Go', 
+'Ke', 'Sin', 'Mon', 
+'Zan', 'Fli', 'Gro', 
+'Han', 'Micro ', 'Daddy ', 
+'The ', 'Lil ', 'Baby ', 
+'King ', 'Man', 'Microsoft ',
+'Big ', 'Mr ', 'Uncle ',
+'Professor ', 'Money '
+];
+
+const middleSyllables = [
+  'bra', 'na', 'ga', 
+  'di', 'so', 're', 
+  'ki', 'li', 'a', 
+  'cha', 'do', 'fer', 
+  'gle', 'shaka', 'man',
+  'gob', 'u', 'i',
+  'e', 'o', ' hot '
+  ];
+
+const endSyllables = [
+  'ne', 'ly', 'vis', 
+  'sa', 'ri', 'ro', 
+  'ko', 'me', 'ton', 
+  'ster', 'ron', 'ko', 
+  'bi', 'bo', 'pasta', 
+  'bing', 'lin',
+  'a', 'e', 'u', 'i',
+  'top', 'chip ',
+  'man'
+  ];
+
+const extraSyllables = [
+  'cust', 'heet', 'low', 
+  'adar', 'chi', 'bon', 
+  'big', 'do', 'fan', 
+  'cul', 'flan', 'nos', 
+  'chi', ' Turbo', ' Stevens',
+  ' Micro', ' Nano', 
+  ' Pro Max', ' 9000', 'man',
+  ' Smith'
+  ];
+
+
+let randomNameEl;
 
 function setup() {
-// Get the container element
   canvasContainer = document.querySelector('#canvasContainer');
   resizeCanvasToMatchContainer();
 
-// Initialize the shirt color components
   shirtR = random(0, 300);
   shirtG = random(0, 300);
   shirtB = random(0, 300);
@@ -33,28 +78,34 @@ for (let i = 1; i < 16; i++) { // only create sliders up to 15
   });
 }
 
+randomNameEl = createP(''); // This creates a new p5 paragraph element (<p>)
+randomNameEl.parent('name-container'); // This will append our p element to the div with id 'name-container'
+setRandomName();
+
+targetValues = sliders.map(slider => random(slider.elt.min, slider.elt.max)); 
+
 // Create and parent the toggle button
 toggleButton = createCheckbox();
 toggleButton.parent('toggleButtonContainer'); 
 
 // Create and parent the randomizer button
-let randomizeButton = createButton('Randomise');
+let randomizeButton = createButton('Generate');
 randomizeButton.parent('randomizeButtonContainer'); 
 
 // Add an event listener to the randomize button
-  randomizeButton.mousePressed(() => {
+randomizeButton.mousePressed(() => {
   targetValues = sliders.map(slider => random(slider.elt.min, slider.elt.max)); 
 
-// Also generate random colors for the shirt
+setRandomName();
 
+// Also generate random colors for the shirt
   shirtR = random(0, 300);
   shirtG = random(0, 300);
   shirtB = random(0, 300);
 
-  // Randomize the state of the toggleButton
+
+// Randomize the state of the toggleButton
   toggleButton.checked(random() > 0.5);
-
-
 });
 }
 
@@ -72,7 +123,6 @@ function draw() {
 let newValue = lerp(currentValue, targetValue, 0.2); // The last parameter is the speed of the transition
 sliders[i].value(newValue);
 }
-
 
 // Then apply translate and scale transformations for the face
 let face_size = Math.min(width, height) / 2.5;
@@ -111,7 +161,6 @@ function resizeCanvasToMatchContainer() {
   }
 }
 
-
 function drawFace4(mouthV, eyeHV, eyeWV, faceWV, jawV, eyeSV, faceCV, eyeCV, noseV, noseBV, hairCV, hairV, mouthHV, eyeRV, beardV, eyeBV) {
 
 //mode setup
@@ -122,52 +171,45 @@ function drawFace4(mouthV, eyeHV, eyeWV, faceWV, jawV, eyeSV, faceCV, eyeCV, nos
 
 //slider maps
   let mouth_size = map(mouthV, 0, 100, 0.5, 2.3);
-  let eyeH = map(eyeHV, 0, 100, 0, 1.5);
-  let eyeW = map(eyeWV, 0, 100, -0.4, 1);
-  let faceW = map(faceWV, 0, 100, 0, 3.5);
+  let eyeY = map(eyeHV, 0, 100, 0, 1.5);
+  let eyeX = map(eyeWV, 0, 100, -0.4, 1);
+  let faceX = map(faceWV, 0, 100, 0, 3.5);
   let jaw = map(jawV, 0, 100, 1, 7);
-  let eyeS = map(eyeSV, 0, 100, 1, 2);
-
-  let faceR = map(faceCV, 0, 100, 0, 15);
-  let faceG = map(faceCV, 0, 100, 30, 20);
-  let faceB = map(faceCV, 0, 100, 20, 70);
-
-  let faceR2 = 5; let faceG2 =  20; let faceB2 = 50;
-
-  let noseR = map(faceCV, 0, 100, 0, 10);
-  let noseG = map(faceCV, 0, 100, 0, 10);
-  let noseB = map(faceCV, 0, 100, 0, 70);
-
-  let noseR2 = faceR; let noseG2 = faceG + 50; let noseB2 = faceB + 50;
-
-  let browR = map(faceCV, 0, 100, 0, 10);
-  let browG = map(faceCV, 0, 100, 0, 10);
-  let browB = map(faceCV, 0, 100, 0, 12);
-
-  let lipsR = map(faceCV, 0, 100, 0, 10);
-  let lipsG = map(faceCV, 0, 100, 0, 10);
-  let lipsB = map(faceCV, 0, 100, 0, 23);
-  let lipsR2 = faceR; let lipsG2 = faceG + 30; let lipsB2 = faceB + 40;
-
-  let eyeR = map(eyeCV, 0, 100, 40, 200);
-  let eyeG = map(eyeCV, 0, 100, 80, 100);
-  let eyeB = map(eyeCV, 0, 100, 40, 100);
-
-
-  let hairR = map(hairCV, 0, 100, 0, 40);
-  let hairG = map(hairCV, 0, 100, 0, 200);
-  let hairB = map(hairCV, 0, 100, 0, 190);
-
-
-
+  let eyeSize = map(eyeSV, 0, 100, 1, 2);
+  let faceHue = map(faceCV, 0, 100, 0, 15); 
+  let faceHue2 = 5; 
+  let faceSaturation = map(faceCV, 0, 100, 30, 20);
+  let faceSaturation2 =  20; 
+  let faceBrightness = map(faceCV, 0, 100, 20, 70);
+  let faceBrightness2 = 50;
+  let noseHue = map(faceCV, 0, 100, 0, 10);
+  let noseHue2 = faceHue; 
+  let noseSaturation = map(faceCV, 0, 100, 0, 10);
+  let noseSaturation2 = faceSaturation + 50; 
+  let noseBrightness = map(faceCV, 0, 100, 0, 70);
+  let noseBrightness2 = faceBrightness + 50;
+  let browHue = map(faceCV, 0, 100, 0, 10);
+  let browSaturation = map(faceCV, 0, 100, 0, 10);
+  let browBrightness = map(faceCV, 0, 100, 0, 12);
+  let lipsHue = map(faceCV, 0, 100, 0, 10);
+  let lipsHue2 = faceHue; 
+  let lipsSaturation = map(faceCV, 0, 100, 0, 10);
+  let lipsSaturation2 = faceSaturation + 30; 
+  let lipsBrightness = map(faceCV, 0, 100, 0, 23);
+  let lipsBrightness2 = faceBrightness + 40;
+  let eyeHue = map(eyeCV, 0, 50, 10, 150);
+  let eyeSaturation = map(eyeCV, 0, 100, 80, 100);
+  let eyeBrightness = map(eyeCV, 20, 100, 40, 100);
+  let hairHue = map(hairCV, 0, 80, 0, 40);
+  let hairSaturation = map(hairCV, 0, 100, 0, 300);
+  let hairBrightness = map(hairCV, 0, 100, 0, 190);
   let nose1 = map(noseV, 0, 100, -1, 2);
   let nose2 = map(noseV, 0, 100, 0.6, 1.3);
   let nose3 = map(noseBV, 0, 100, 1.2, 2);
   let nose4 = map(noseBV, 0, 100, 0, 1);
-
   let hairCut = map(hairV, 0, 100, 0, 6);
   let mouthH = map(mouthHV, 0, 100, -1, 1);
-  let eyeRot = map(eyeRV, 0, 100, -5, 5);
+  let eyeRotate = map(eyeRV, 0, 100, -5, 5);
   let beardW = map(jawV, 0, 100, 1, -5);
   let beardCut = map(beardV, 0, 100, 0, 10);
   let eyeBag = map(eyeBV, 0, 100, 0, 10);
@@ -176,90 +218,95 @@ function drawFace4(mouthV, eyeHV, eyeWV, faceWV, jawV, eyeSV, faceCV, eyeCV, nos
   push();
   colorMode(RGB);
   fill(shirtR-80, shirtG-80, shirtB-80);
-  rect(0,13,faceW*5 + 10,15,10);
+  rect(0,13,faceX*5 + 10,15,10);
+  rect(0,20,faceX*5 + 10,15);
+  pop();
+  fill(faceHue2+faceHue, faceSaturation2+faceSaturation, faceBrightness2+faceBrightness);
+  rect(0,22,faceX*5 + 10,15);
+  push();
+  colorMode(RGB);
+  fill(shirtR-80, shirtG-80, shirtB-80);
+  rect(0,20,faceX*2 + 10,19);
   pop();
 
-  fill(faceR2-20+faceR, faceG2-20+faceG, faceB2-20+faceB);
+  fill(faceHue2-20+faceHue, faceSaturation2-20+faceSaturation, faceBrightness2-20+faceBrightness);
   ellipse(0,5,7.5,11);
 
 // head
-  fill(faceR2+faceR, faceG2+faceG, faceB2+faceB);
-  rect(0, 0, 9.5+faceW, 18, 10,10,jaw,jaw);
-
-
+  fill(faceHue2+faceHue, faceSaturation2+faceSaturation, faceBrightness2+faceBrightness);
+  rect(0, 0, 9.5+faceX, 18, 10,10,jaw,jaw);
 
 //hair
-  fill(0+hairR, 0+hairG, 0+hairB);
+  fill(0+hairHue, 0+hairSaturation, 0+hairBrightness);
   if (hairCut >= 1){
-    rect(-5-faceW/2,-2,2,4, 1);
-    rect(5+faceW/2,-2,2,4, 1);
+    rect(-5-faceX/2,-2,2,4, 1);
+    rect(5+faceX/2,-2,2,4, 1);
   }
   if (hairCut >= 2){
-    ellipse(-5-faceW/2,-3,2,4, 1);
-    ellipse(5+faceW/2,-3,2,4, 1);
-    ellipse(0,-8.2,4+faceW,3);
+    ellipse(-5-faceX/2,-3,2,4, 1);
+    ellipse(5+faceX/2,-3,2,4, 1);
+    ellipse(0,-8.2,4+faceX,3);
   }  
   if (hairCut >=3){
-    ellipse(-4.75-faceW/2,-5,3,6, 1);
-    ellipse(4.75+faceW/2,-5,3,6, 1);
-    ellipse(0,-7.2,11+faceW,5);
+    ellipse(-4.75-faceX/2,-5,3,6, 1);
+    ellipse(4.75+faceX/2,-5,3,6, 1);
+    ellipse(0,-7.2,11+faceX,5);
 
   }
   if (hairCut >=4){
-    rect(-4.5-faceW/2,-3,1.5,7, 1);
-    rect(4.5+faceW/2,-3,1.5,7, 1);
-    ellipse(2,-5,5+faceW,3);
+    rect(-4.5-faceX/2,-3,1.5,7, 1);
+    rect(4.5+faceX/2,-3,1.5,7, 1);
+    ellipse(2,-5,5+faceX,3);
   }
   if (hairCut >=5){
-    rect(-4.5-faceW/2,-2.5,1.5,10, 1);
-    rect(4.5+faceW/2,-2.5,1.5,10, 1);
-    ellipse(-5,-6.5,3+faceW,3+faceW);
-    ellipse(5,-6.5,3+faceW,3+faceW);
-    ellipse(4,-7.5,3+faceW,3+faceW);
-    ellipse(-4,-7.5,3+faceW,3+faceW);
-    ellipse(0,-8,5+faceW,5+faceW);
-
+    rect(-4.5-faceX/2,-2.5,1.5,10, 1);
+    rect(4.5+faceX/2,-2.5,1.5,10, 1);
+    ellipse(-5,-6.5,3+faceX,3+faceX);
+    ellipse(5,-6.5,3+faceX,3+faceX);
+    ellipse(4,-7.5,3+faceX,3+faceX);
+    ellipse(-4,-7.5,3+faceX,3+faceX);
+    ellipse(0,-8,5+faceX,5+faceX);
   }
 
 //eyebrows
-  fill(0+browR, 0+browG, 0+browB);
-  rect(-2.5-eyeW, -3.1+eyeH/2, 2.5+eyeW, 0.7, 0, 1,0,0);
-  rect( 2.5+eyeW, -3.1+eyeH/2, 2.5+eyeW, 0.7, 1, 0,0,0);
+  fill(0+browHue, 0+browSaturation, 0+browBrightness);
+  rect(-2.5-eyeX, -3.1+eyeY/2, 2.5+eyeX, 0.7, 0, 1,0,0);
+  rect( 2.5+eyeX, -3.1+eyeY/2, 2.5+eyeX, 0.7, 1, 0,0,0);
 
 //ears
-  fill(faceR2+faceR, faceG2+faceG, faceB2+faceB);
-  ellipse(-5.2-faceW/2, 0, 2, 2.3);
-  ellipse(-5-faceW/2, 0.8, 1.5, 2);
-  ellipse(5.2+faceW/2, 0, 2, 2.3);
-  ellipse(5+faceW/2, 0.8, 1.5, 2);
-  fill(noseR2+noseR, noseG2+noseG, noseB2+noseB);
-  ellipse(5.2+faceW/2, 0.3, 0.6,1.5);
-  ellipse(-5.2-faceW/2, 0.3, 0.6,1.5);
+  fill(faceHue2+faceHue, faceSaturation2+faceSaturation, faceBrightness2+faceBrightness);
+  ellipse(-5.2-faceX/2, 0, 2, 2.3);
+  ellipse(-5-faceX/2, 0.8, 1.5, 2);
+  ellipse(5.2+faceX/2, 0, 2, 2.3);
+  ellipse(5+faceX/2, 0.8, 1.5, 2);
+  fill(noseHue2+noseHue, noseSaturation2+noseSaturation, noseBrightness2+noseBrightness);
+  ellipse(5.2+faceX/2, 0.3, 0.6,1.5);
+  ellipse(-5.2-faceX/2, 0.3, 0.6,1.5);
 
 // eyes
   if(eyeBag >= 8){
-    fill(noseR2+noseR, noseG2+noseG, noseB2+noseB);
-    ellipse(-2.5-eyeW, -1+eyeH, 2, 3);
-    ellipse(2.5+eyeW, -1+eyeH, 2, 3);
+    fill(noseHue2+noseHue, noseSaturation2+noseSaturation, noseBrightness2+noseBrightness);
+    ellipse(-2.5-eyeX, -1+eyeY, 2, 3);
+    ellipse(2.5+eyeX, -1+eyeY, 2, 3);
   }
-  fill(faceR2+faceR, faceG2+faceG, faceB2+faceB);
-  ellipse(-2.5-eyeW, -1.2+eyeH, 2, 3);
-  ellipse(2.5+eyeW, -1.2+eyeH, 2, 3);
-  fill(lipsR2+lipsR, lipsG2+lipsG, lipsB2+lipsB); 
-  ellipse(-2.5-eyeW, -1.7+eyeH, 2, 2);
-  ellipse( 2.5+eyeW, -1.7+eyeH, 2, 2);
+  fill(faceHue2+faceHue, faceSaturation2+faceSaturation, faceBrightness2+faceBrightness);
+  ellipse(-2.5-eyeX, -1.2+eyeY, 2, 3);
+  ellipse(2.5+eyeX, -1.2+eyeY, 2, 3);
+  fill(lipsHue2+lipsHue, lipsSaturation2+lipsSaturation, lipsBrightness2+lipsBrightness); 
+  ellipse(-2.5-eyeX, -1.7+eyeY, 2, 2);
+  ellipse( 2.5+eyeX, -1.7+eyeY, 2, 2);
   fill(240);
   push();
-  rotate(eyeRot);
-  rect(-2.5-eyeW, -1.7+eyeH, 2, eyeS, eyeS);
+  rotate(eyeRotate);
+  rect(-2.5-eyeX, -1.7+eyeY, 2, eyeSize, eyeSize);
   pop();
   push();
-  rotate(-eyeRot);
-  rect( 2.5+eyeW, -1.7+eyeH, 2, eyeS, eyeS);
+  rotate(-eyeRotate);
+  rect( 2.5+eyeX, -1.7+eyeY, 2, eyeSize, eyeSize);
   pop();
-  fill(eyeR, eyeG, eyeB);
-  ellipse(-2.5-eyeW, -1.7+eyeH, 1.1, 1.1);
-  ellipse( 2.5+eyeW, -1.7+eyeH, 1.1, 1.1);
+  fill(eyeHue, eyeSaturation, eyeBrightness);
+  ellipse(-2.5-eyeX, -1.7+eyeY, 1.1, 1.1);
+  ellipse( 2.5+eyeX, -1.7+eyeY, 1.1, 1.1);
 
 
 //beard
@@ -267,38 +314,35 @@ function drawFace4(mouthV, eyeHV, eyeWV, faceWV, jawV, eyeSV, faceCV, eyeCV, nos
     push();
     colorMode(RGB);
     fill(0,0,0,40);
-    rect(0,6+(mouthH/2),8.5+faceW+beardW,6-mouthH,jaw);
+    rect(0,6+(mouthH/2),8.5+faceX+beardW,6-mouthH,jaw);
     pop();
   }
   if (beardCut >= 4){
-    fill(hairR-10, hairG-10, hairB-10);
-    rect(0,mouthH+4,6+faceW+(beardW/1.5),2,jaw);
+    fill(hairHue-10, hairSaturation-10, hairBrightness-10);
+    rect(0,mouthH+4,6+faceX+(beardW/1.5),2,jaw);
   }
   if (beardCut >= 6){
-    fill(hairR-10, hairG-10, hairB-10);
-    rect(0,6+(mouthH/2),8.5+faceW+beardW,6-mouthH,jaw);
+    fill(hairHue-10, hairSaturation-10, hairBrightness-10);
+    rect(0,6+(mouthH/2),8.5+faceX+beardW,6-mouthH,jaw);
   }
   if (beardCut >= 8){
-    fill(hairR-10, hairG-10, hairB-10);
-    rect(0,6+(mouthH/4),9.5+faceW,6-(mouthH/2),jaw+2);
+    fill(hairHue-10, hairSaturation-10, hairBrightness-10);
+    rect(0,6+(mouthH/4),9.5+faceX,6-(mouthH/2),jaw+2);
   } 
-  fill(faceR2+faceR, faceG2+faceG, faceB2+faceB);
+  fill(faceHue2+faceHue, faceSaturation2+faceSaturation, faceBrightness2+faceBrightness);
   rect(0,5+mouthH,4.5,mouth_size+0.5,2);
 
-
-
 // nose
-  fill(noseR2+noseR, noseG2+noseG, noseB2+noseB);
+  fill(noseHue2+noseHue, noseSaturation2+noseSaturation, noseBrightness2+noseBrightness);
   rect(0,0.4, nose3,3+nose4);
   rect(0, 2.35, 2.95, nose2, 1);
   ellipse(0, 2.1, 2+nose1,2.2);
 
 //mouth 
-  fill(lipsR2+lipsR, lipsG2+lipsG, lipsB2+lipsB);
+  fill(lipsHue2+lipsHue, lipsSaturation2+lipsSaturation, lipsBrightness2+lipsBrightness);
   rect(0, 5+mouthH, 4, mouth_size, 1);
   fill(0);
   rect(0, 5+mouthH, 3.4,0.1,0.6);
-
 }
 
 function resetFocusedRandom() {
@@ -330,5 +374,21 @@ function focusedRandom(min, max, focus, mean) {
   }
   return d3.randomUniform(min, max)();
 }
+
+function setRandomName() {
+  const syllables = [startSyllables, middleSyllables, endSyllables, extraSyllables];
+  
+  let numSyllables = Math.floor(Math.random() * 3) + 2; // Choose two to four syllables
+
+  let name = '';
+  for (let i = 0; i < numSyllables; i++) {
+    let syllableList = syllables[i];
+    let syllable = syllableList[Math.floor(Math.random() * syllableList.length)];
+    name += syllable;
+  }
+
+  randomNameEl.html(`${name}`);
+}
+
 
 
